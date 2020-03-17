4473300bd144cb879f752a3906955d1416f01732
tuan-brother <phungtuananhxy@gmail.com>
2020-03-17 23:36:53 +0700

    lam ở nha chat

--- app/src/main/java/com/example/appchatfb/ActivityChat.java
+++ app/src/main/java/com/example/appchatfb/ActivityChat.java
@@ -5,19 +5,62 @@ import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.appchatfb.databinding.ActivityChatBinding;
+import com.example.appchatfb.generated.callback.OnClickListener;
+import com.example.appchatfb.model.ChatMessage;
 import com.example.appchatfb.model.User;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ActivityChat extends AppCompatActivity {
+    private FirebaseListAdapter<ChatMessage> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityChatBinding binding= DataBindingUtil.setContentView(this,R.layout.activity_chat);
         setSupportActionBar(binding.tbChatfriend);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Bundle bundle=getIntent().getBundleExtra("bundle");
         User user=new User(null,null,bundle.getString("name"),bundle.getString("image"),null,null);
         binding.setUser(user);
+        binding.btnSendmessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase.getInstance()
+                        .getReference().child("CSDL").child("Chat").child(bundle.getString("name")).push()
+                        .setValue(new ChatMessage(binding.edtMessage.getText().toString(),
+                                FirebaseAuth.getInstance()
+                                        .getCurrentUser()
+                                        .getDisplayName())
+                        );
+                Toast.makeText(ActivityChat.this, "Chay đến đây rồi", Toast.LENGTH_SHORT).show();
+                binding.edtMessage.setText("");
+            }
+        });
+
+        adapter=new FirebaseListAdapter<ChatMessage>(this,ChatMessage.class,R.layout.message,FirebaseDatabase.getInstance().getReference("CSDL/Chat/"+bundle.getString("name"))) {
+            @Override
+            protected void populateView(View v, ChatMessage model, int position) {
+                TextView messageText = (TextView)v.findViewById(R.id.message_text);
+                TextView messageUser = (TextView)v.findViewById(R.id.message_user);
+                TextView messageTime = (TextView)v.findViewById(R.id.message_time);
+
+                // Set their text
+                messageText.setText(model.getMessageText());
+                messageUser.setText(model.getMessageUser());
+
+                // Format the date before showing it
+                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)",
+                        model.getMessageTime()));
+            }
+        };
+        binding.rcFmChat.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/appchatfb/model/itemChat.java
+++
@@ -1,5 +0,0 @@
-package com.example.appchatfb.model;
-
-public class itemChat {
-    String conTent,anh,time;
-}
